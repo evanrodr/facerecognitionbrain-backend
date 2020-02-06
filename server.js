77184ded945cae2ab6e95932af98164fb3b3285c
Evandro Rodrigues <43556689+evanrodr@users.noreply.github.com>
2020-02-06 14:27:15 -0300

    fixing security issues and getting ready to deploy

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const register = require('./controllers/Register');
+const signin = require('./controllers/Signin');
+const profile = require('./controllers/Profile');
+const image = require('./controllers/Image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'postgres',
+    password: 'erodr9821',
+    database: 'smart-brain'
+  }
+});
+
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+})
